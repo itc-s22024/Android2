3ce4f0c4e305e45c3c4bb645f98e67d165152b9a
s22024 <s22024@std.it-college.ac.jp>
2023-10-05 11:21:43 +0900

    天気の詳細が取れるようになったぞ

--- AsyncSample/app/src/main/java/jp/ac/it_college/std/s22024/async_sample/MainActivity.kt
+++ AsyncSample/app/src/main/java/jp/ac/it_college/std/s22024/async_sample/MainActivity.kt
@@ -2,10 +2,86 @@ package jp.ac.it_college.std.s22024.async_sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.UiThread
+import androidx.annotation.WorkerThread
+import androidx.recyclerview.widget.LinearLayoutManager
+import jp.ac.it_college.std.s22024.async_sample.databinding.ActivityMainBinding
+import org.json.JSONObject
+import java.net.HttpURLConnection
+import java.net.SocketTimeoutException
+import java.net.URL
+import java.util.concurrent.Callable
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val DEBUG_TAG = "AsyncSample"
+        private const val WEATHER_INFO_URL = "https://api.openweathermap.org/data/2.5/weather?lang=ja"
+        private const val APP_ID = BuildConfig.APP_ID
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvCityList.apply {
+            adapter = CityAdapter {
+                receiveWeatherInfo(it.q)
+            }
+            layoutManager = LinearLayoutManager(context)
+        }
+    }
+    @UiThread
+    private fun receiveWeatherInfo(q: String) {
+        val url = "$WEATHER_INFO_URL&q=$q&appid=$APP_ID"
+        val executorService = Executors.newSingleThreadExecutor()
+        val backgroundReceiver = WeatherIndoBackgroundReceiver(url)
+        val future = executorService.submit(backgroundReceiver)
+        val result = future.get()
+        showWeatherInfo(result)
+    }
+
+    @UiThread
+    private fun showWeatherInfo(result: String) {
+        val root = JSONObject(result)
+        val cityName = root.getString("name")
+        val coord = root.getJSONObject("coord")
+        val latitude = coord.getDouble("lat")
+        val longitude = coord.getDouble("lon")
+        val weatherArray = root.getJSONArray("weather")
+        val current = weatherArray.getJSONObject(0)
+        val weather = current.getString("description")
+
+        binding.tvWeatherTelop.text = getString(R.string.tv_telop, cityName)
+        binding.tvWeatherDesc.text = getString(
+            R.string.tv_desc, weather, latitude, longitude
+        )
+    }
+
+    private class WeatherIndoBackgroundReceiver(val urlString: String) : Callable<String> {
+        @WorkerThread
+        override fun call(): String {
+            val url = URL(urlString)
+            val con = url.openConnection() as HttpURLConnection
+            con.apply {
+                connectTimeout = 1000
+                readTimeout = 1000
+                requestMethod = "GET"
+            }
+            return try{
+                con.connect()
+                val result = con.inputStream.reader().readText()
+                con.disconnect()
+                result
+            } catch (ex: SocketTimeoutException) {
+                Log.w(DEBUG_TAG, "通信タイムアウト", ex)
+                ""
+            }
+        }
     }
 }
