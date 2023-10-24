9d1b26e85c7a27fdc8e07c0c58f08c4abb929060
s22024 <s22024@std.it-college.ac.jp>
2023-10-24 09:46:59 +0900

    first commit

--- ImplicitintentSample/app/src/main/java/jp/ac/it_college/std/s22024/implicit_intent_sample/MainActivity.kt
+++ ImplicitintentSample/app/src/main/java/jp/ac/it_college/std/s22024/implicit_intent_sample/MainActivity.kt
@@ -1,11 +1,120 @@
 package jp.ac.it_college.std.s22024.implicit_intent_sample
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import jp.ac.it_college.std.s22024.implicit_intent_sample.databinding.ActivityMainBinding
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+
+    private val locationPermissionRequest = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val isGrantedFineLocation =
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false)
+        val isGrantedCoarseLocation =
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false)
+
+        if(isGrantedFineLocation || isGrantedCoarseLocation) {
+            requestLocationUpdates()
+            return@registerForActivityResult
+        }
+        Log.w("CHAPTER14", "権限よこせやカス")
+    }
+
+    private var latitude = 0.0
+    private var longitude = 0.0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btMapSearch.setOnClickListener(::onMapSearchButton)
+        binding.btMapShowCurrent.setOnClickListener(::onMapShowCurrentButtonClick)
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY, 5000
+        ).build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                result.lastLocation?.also {location ->
+                    latitude = location.latitude
+                    longitude = location.longitude
+
+                    binding.tvLatitude.text = latitude.toString()
+                    binding.tvLongitude.text = longitude.toString()
+                }
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onResume() {
+        super.onResume()
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, mainLooper)
+    }
+
+    override fun onPause() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        super.onPause()
+    }
+
+    private fun onMapSearchButton(view: View) {
+        val searchWord = URLEncoder.encode(
+            binding.etSearchWord.text.toString(), "UTF-8"
+        )
+        val uri = Uri.parse("geo:0,0?q=${searchWord}")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+    private fun onMapShowCurrentButtonClick(view: View) {
+        val uri = Uri.parse("geo:${latitude},${longitude}")
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+
+    private fun requestLocationUpdates() {
+        val isGrantedFineLocation = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val isGrantedCoarseLocation = ContextCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (isGrantedFineLocation || isGrantedCoarseLocation) {
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                mainLooper
+            )
+            return
+        }
+
+        locationPermissionRequest.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+        )
     }
 }
